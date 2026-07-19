52350ce9094188d1dc703ea0197c10c86e70a8b5
tanmay5268 <tanmay.mewati94@gmail.com>
2026-07-19 19:52:57 +0530

    Home Page design, Signin not done yet

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,18 @@
-
+import Navbar from "@/components/HeroPage/Navbar"
+import Hero from "@/components/HeroPage/Hero";
+import Footer from "@/components/HeroPage/Footer";
 // Server components using auth methods must be rendered dynamically
 export const dynamic = 'force-dynamic';
 
 export default async function Home() {
 
   return (
-    <div className="flex flex-col gap-2 min-h-screen items-center justify-center bg-gray-900">
-     Hello
+    <div className="relative overflow-hidden flex min-h-screen w-full flex-col">
+      <Navbar />
+      <Hero />
+      <div className="mt-auto">
+        <Footer />
+      </div>
     </div>
   );
 }
